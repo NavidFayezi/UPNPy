eb64ecf1ffd63e04e057de3c271febca2d8c6072
Navid Fayezi <navidfayezi.98@gmail.com>
2022-06-23 13:04:50 +0430

    Add select_gateway function

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import upnpy
 
 
-upnp = upnpy.UPnP()
-devices = upnp.discover()
-device = devices[0]
+def select_gateway():
+    upnp = upnpy.UPnP()
+    devices = upnp.discover()
+    if len(devices) == 0:
+        raise RuntimeError("No gateway was found. Try again.")
+    i = 0
+    for d in devices:
+        print(i, "- ", d)
+        i += 1
+    index = int(input("Please select your gateway, enter a number: "))
+    device = devices[index]
+    return device
 
-service = device['WANPPPConn1']
 
-print(service.get_actions())
+def main(device):
+    a = device.services.keys()
+    print(list(a))
+    service = device['WANPPPConn1']
 
-# Finally, get the external IP address
-# Execute the action by its name
-# Returns a dictionary: {'NewExternalIPAddress': 'xxx.xxx.xxx.xxx'}
-print(service.GetExternalIPAddress())
+    print(service.get_actions())
 
-#print(service.AddPortMapping.get_input_arguments())
-print(service.DeletePortMapping.get_input_arguments())
-"""service.AddPortMapping(
-    NewRemoteHost='',
-    NewExternalPort=21180,
-    NewProtocol='TCP',
-    NewInternalPort=22000,
-    NewInternalClient='192.168.1.104',
-    NewEnabled=1,
-    NewPortMappingDescription='Test port mapping entry from UPnPy.',
-    NewLeaseDuration=0
-)"""
-service.DeletePortMapping(
-    NewRemoteHost='',
-    NewExternalPort=21180,
-    NewProtocol='TCP')
+    # Finally, get the external IP address
+    # Execute the action by its name
+    # Returns a dictionary: {'NewExternalIPAddress': 'xxx.xxx.xxx.xxx'}
+    print(service.GetExternalIPAddress())
+
+    #print(service.AddPortMapping.get_input_arguments())
+    print(service.DeletePortMapping.get_input_arguments())
+    """service.AddPortMapping(
+        NewRemoteHost='',
+        NewExternalPort=21180,
+        NewProtocol='TCP',
+        NewInternalPort=22000,
+        NewInternalClient='192.168.1.104',
+        NewEnabled=1,
+        NewPortMappingDescription='Test port mapping entry from UPnPy.',
+        NewLeaseDuration=0
+    )"""
+    """service.DeletePortMapping(
+        NewRemoteHost='',
+        NewExternalPort=21180,
+        NewProtocol='TCP')"""
+
+
+main(select_gateway())
