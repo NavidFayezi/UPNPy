942febc20725ffa05fed3dde2c8897ab5b8f1c7a
Navid <navidfayezi.98@gmail.com>
2022-07-01 06:10:15 -0400

    Refactor

--- main.py
+++ main.py
@@ -1,57 +1,55 @@
-from rx import return_value
 import upnpy
 
 
 def select_gateway():
-    upnp = upnpy.UPnP()
-    devices = upnp.discover()
-    device = upnp.get_igd()
-    """if len(devices) == 0:
-        raise RuntimeError("No gateway was found. Try again.")
-    i = 0
-    for d in devices:
-        print(i, "- ", d)
-        i += 1
-    index = int(input("Please select your gateway, enter a number: "))
-    while index < 0 or index >= len(devices):
-        index = int(input("Error. Please select a valid number corresponding to your gateway: "))
-    device = devices[index]
-    """
-    return device
+    try:
+        upnp = upnpy.UPnP()
+        devices = upnp.discover()
+        device = upnp.get_igd()
+        return device
+
+    except Exception as err:
+        print("Erro while discovering the gateway.")
+        print(err)
 
 
-def main(device):
-    service_name = list(device.services.keys())[0]
-    service = device[service_name]
+def main():
+    try:
+        device = select_gateway()
+        service_name = list(device.services.keys())[0]
+        service = device[service_name]
 
-    print(service.get_actions())
+        print(service.get_actions())
 
-    # Finally, get the external IP address
-    # Execute the action by its name
-    # Returns a dictionary: {'NewExternalIPAddress': 'xxx.xxx.xxx.xxx'}
-    print(get_external_ip(service))
+        # Finally, get the external IP address
+        # Execute the action by its name
+        # Returns a dictionary: {'NewExternalIPAddress': 'xxx.xxx.xxx.xxx'}
+        print(get_external_ip(service))
 
-    #print(service.AddPortMapping.get_input_arguments())
-    #print(service.DeletePortMapping.get_input_arguments())
-    #print("here ", service.GetSpecificPortMappingEntry(NewRemoteHost='', NewExternalPort=21181, NewProtocol='TCP'))
-    
-    """service.AddPortMapping(
-        NewRemoteHost='',
-        NewExternalPort=21180,
-        NewProtocol='TCP',
-        NewInternalPort=22000,
-        NewInternalClient='192.168.1.149',
-        NewEnabled=1,
-        NewPortMappingDescription='Test port mapping entry from UPnPy.',
-        NewLeaseDuration=0
-    )"""
-    #print("whatch this: ", service.GetGenericPortMappingEntry(NewPortMappingIndex=0))
-    
-    """service.DeletePortMapping(
-        NewRemoteHost='',
-        NewExternalPort=21180,
-        NewProtocol='TCP')
-       """ 
+        #print(service.AddPortMapping.get_input_arguments())
+        #print(service.DeletePortMapping.get_input_arguments())
+        #print("here ", service.GetSpecificPortMappingEntry(NewRemoteHost='', NewExternalPort=21181, NewProtocol='TCP'))
+        
+        """service.AddPortMapping(
+            NewRemoteHost='',
+            NewExternalPort=21180,
+            NewProtocol='TCP',
+            NewInternalPort=22000,
+            NewInternalClient='192.168.1.149',
+            NewEnabled=1,
+            NewPortMappingDescription='Test port mapping entry from UPnPy.',
+            NewLeaseDuration=0
+        )"""
+        #print("whatch this: ", service.GetGenericPortMappingEntry(NewPortMappingIndex=0))
+        
+        """service.DeletePortMapping(
+            NewRemoteHost='',
+            NewExternalPort=21180,
+            NewProtocol='TCP')
+        """ 
+    except Exception as err:
+        print(err)
+
 def add_port_mapping( service,external_port, protocol, internal_port, client_ip, description='', lease_duration=0):
     try:
         service.AddPortMapping(
@@ -65,9 +63,11 @@ def add_port_mapping( service,external_port, protocol, internal_port, client_ip,
         NewLeaseDuration=lease_duration
         )
 
-    except Exception:
+    except Exception as err:
         print("Error while adding the portmapping.")
+        print(err)
         
+
 def get_external_ip(service):
     try:
 
@@ -76,4 +76,6 @@ def get_external_ip(service):
     except Exception as err:
         print(err)
 
-main(select_gateway())
+
+if __name__ == "__main__":
+    main()
